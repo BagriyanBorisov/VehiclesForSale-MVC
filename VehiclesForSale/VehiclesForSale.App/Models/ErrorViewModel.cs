1ed3442f962e8a184d634d0b4f1e4c25b423e24d
BagriyanBorisov <bagriyan.dilyanov@abv.bg>
2023-06-19 13:23:11 +0300

    Initial commit

---
+++ VehiclesForSale/VehiclesForSale.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehiclesForSale.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
